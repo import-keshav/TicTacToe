2a699fdc4aa506dee42b5efa0160025d4a754588
Keshav Bathla <keshavbathla2017@gmail.com>
2019-07-25 15:21:22 +0530

    complete 2 players game

---
+++ home.js
@@ -0,0 +1,116 @@
+var player_1_score = 0;
+var player_2_score = 0;
+var tie_score = 0;
+var computer_score = 0;
+
+var response_sequense = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+var game_counter = 0;
+
+var increment_player_score = function(score) {
+  var score_object = document.createElement("span");
+  score = score.toString();
+  score_object.innerHTML = score;
+  $(score_object).css({
+    "left": "350px",
+    "top": "450px",
+    "color": "white",
+    "position": "absolute",
+    "font-size": "30px"
+  });
+};
+
+
+var possible_ways  = {
+  '1': ['23', '47', '59'],
+  '2': ['13', '58'],
+  '3': ['69', '12', '57'],
+  '4': ['17', '56'],
+  '5': ['28', '46', '19', '37'],
+  '6': ['39', '54'],
+  '7': ['14', '53', '89'],
+  '8': ['52', '79'],
+  '9': ['63', '15', '87']
+}
+
+
+var restart = function() {
+  $(".cross_and_circle").remove();
+  game_counter = 0;
+}
+
+
+var is_win = function(box_id) {
+  var to_check = $('#' + box_id)[0].innerText;
+  var num_id = box_id.split("box_")[1];
+  var num_of_ways = possible_ways[num_id];
+  for (way in num_of_ways) {
+      ids = num_of_ways[way].split('');
+      try {
+        if ($('#box_' + ids[0]).children()[0].innerText === to_check &&
+          $('#box_' + ids[1]).children()[0].innerText === to_check) {
+          return true;
+        }
+      }
+      catch(err) {
+      }
+  }
+};
+
+
+var append_cross_or_circle = function(box_id) {
+  if ($('#' + box_id).children().length === 0) {
+    var cross_and_circle_object = document.createElement("span");
+    cross_and_circle_object.innerHTML = response_sequense[game_counter];
+    game_counter += 1;
+    $(cross_and_circle_object).css({
+      "font-size": "60px",
+      "margin-left": "10px",
+      "color": "white"
+    });
+  }
+  $('#' + box_id).append(cross_and_circle_object);
+  $(cross_and_circle_object).addClass('cross_and_circle');
+};
+
+
+var update_score = function(player) {
+  if (player === "player_1") {
+    alert("Player 1 wins");
+    player_1_score += 1;
+    $(".player_1_score_number").html(player_1_score);
+    return;
+  }
+  else if (player === "player_2") {
+    alert("Player 2 wins");
+    player_2_score += 1;
+    $(".player_2_score_number").html(player_2_score);
+    return;
+  }
+  alert("Game is TIE");
+  tie_score += 1;
+  $(".tie_score_number").html(tie_score);
+};
+
+
+var enter_player_response = function(obj) {
+  var box_id = $(obj).attr('id');
+  append_cross_or_circle(box_id);
+
+  if (is_win(box_id)) {
+    var who_win = $('#' + box_id)[0].innerText;
+
+    if(who_win === "X") {
+      update_score("player_1")
+      restart();
+      return;
+    }
+    update_score("player_2")
+    restart();
+    return;
+  }
+
+  if(game_counter === 9) {
+    update_score("tie")
+    restart();
+  }
+};
---
+++ setting_no_of_players.js
@@ -0,0 +1,25 @@
+var number_of_players = function() {
+  var options_of_num_of_players = document.getElementById("number_of_player");
+  var num_of_players = (
+    options_of_num_of_players.options[options_of_num_of_players.selectedIndex].value);
+  if (num_of_players === 'computer') {
+    return "1"
+  }
+  return "2"
+};
+
+
+var change_player_score_text = function() {
+  if (number_of_players() === "2") {
+    $("#player_2_score").css('visibility','visible');
+    $("#computer_score").css('visibility','hidden');
+    return
+  }
+  $("#player_2_score").css('visibility','hidden');
+  $("#computer_score").css('visibility','visible');
+};
+
+
+$(document).on('change', '#number_of_player', function() {
+  change_player_score_text()
+})
